2e662ae879afeefe944442cf1e03478c1c334042
b1f6c1c4 <b1f6c1c4@gmail.com>
2021-10-20 15:00:58 -0400

    Improve buffer stage

--- hw/boards/ML605/chnl/buffer.v
+++ hw/boards/ML605/chnl/buffer.v
@@ -8,23 +8,64 @@ module buffer #(
    output i_rdy,
    input [WIDTH-1:0] i_data,
 
-   output reg o_val,
+   output o_val,
    input o_rdy,
-   output reg [WIDTH-1:0] o_data
+   output [WIDTH-1:0] o_data
 );
 
-   wire en;
+   localparam S_AOBI = 1'b0;
+   localparam S_AIBO = 1'b1;
 
-   assign i_rdy = o_rdy || !o_val;
-   assign en = i_rdy && i_val;
+   reg state, state_next;
+
+   reg oA_val, oB_val;
+   reg [WIDTH-1:0] oA_data, oB_data;
+
+   // For timing requirement, i_rdy must NOT depend on o_rdy.
+   assign i_rdy = !rst && (state == S_AIBO ? !oA_val : !oB_val);
+   assign o_val = state == S_AOBI ? oA_val : oB_val;
+   assign o_data = state == S_AOBI ? oA_data : oB_data;
+
+   always @(*) begin
+      state_next = state;
+      // Toggle state iff. the output buffer is clear
+      if (!o_val || o_rdy) begin
+         case (state)
+            S_AOBI: state_next = S_AIBO;
+            S_AIBO: state_next = S_AOBI;
+         endcase
+      end
+   end
 
    always @(posedge clk, posedge rst) begin
       if (rst) begin
-         o_val <= 0;
-         o_data <= 0;
-      end else if (en) begin
-         o_val <= i_val;
-         o_data <= i_data;
+         state <= S_AOBI;
+      end else begin
+         state <= state_next;
+      end
+   end
+
+   always @(posedge clk, posedge rst) begin
+      if (rst) begin
+         oA_val <= 0;
+         oA_data <= 0;
+      end else if (state == S_AIBO && !oA_val) begin // take input
+         oA_val <= i_val;
+         oA_data <= i_data;
+      end else if (state == S_AOBI && o_rdy) begin // emit output
+         oA_val <= 0;
+      end
+   end
+
+   always @(posedge clk, posedge rst) begin
+      if (rst) begin
+         oB_val <= 0;
+         oB_data <= 0;
+      end else if (state == S_AOBI && !oB_val) begin // take input
+         oB_val <= i_val;
+         oB_data <= i_data;
+      end else if (state == S_AIBO && o_rdy) begin // emit output
+         oB_val <= 0;
       end
    end
 
